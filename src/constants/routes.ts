2a4dbadd718285f243df22517366e8c751a06e8c
vlad-macos <vladislav@MacBook-Air-Vladislav.local>
2022-08-17 14:35:58 +0300

    route constants file was added

---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+/* Movies */
+export const moviesListRoute = '/'
+export const moviesDetailRoute = '/:id'
+export const buildMoviesDetailRoute = (movieId: string) => `/${movieId}`
+
+/* Genres */
--- src/view/pages/movies/router.tsx
+++ src/view/pages/movies/router.tsx
@@ -1,8 +1,9 @@
 import { Route } from 'react-router-dom'
 import MoviesListFacade from 'view/pages/movies/list/ui'
 import MovieDetailFacade from 'view/pages/movies/detail/ui'
+import { moviesDetailRoute, moviesListRoute } from 'constants/routes'
 
 export default [
-  <Route path="/" element={<MoviesListFacade />} />,
-  <Route path="/detail" element={<MovieDetailFacade />} />,
+  <Route path={moviesListRoute} element={<MoviesListFacade />} />,
+  <Route path={moviesDetailRoute} element={<MovieDetailFacade />} />,
 ]
